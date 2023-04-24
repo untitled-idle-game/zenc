445789a3d6701c831ad88abace6f77c9ee430325
watsonet <watsonet@rose-hulman.edu>
2023-04-24 14:47:03 -0400

    Make start script cross-platform

--- serve.sh
+++ serve.sh
@@ -1,8 +1,6 @@
+#!/usr/bin/pwsh
+
+echo $SHELL
 tsc --project ./functions
 rm -rf ./public/*
-concurrently -k \
-            "firebase serve" \
-            "tsc --project ./functions --watch" \
-            "cd frontend && ./node_modules/.bin/vue-cli-service build --no-clean --dest ../public --watch" \
-            "cd frontend && ./node_modules/.bin/vue-cli-service serve" \
-            "sleep 10 && browser-sync start --proxy 'localhost:5000' --files 'public/*'"
+concurrently -k "firebase serve" "tsc --project ./functions --watch" "cd frontend && ./node_modules/.bin/vue-cli-service build --no-clean --dest ../public --watch" "cd frontend && ./node_modules/.bin/vue-cli-service serve" "sleep 10 && browser-sync start --proxy 'localhost:5000' --files 'public/*'"
