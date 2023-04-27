e93aa701a36a030eb61712cbeb56ff20467386f4
rhit-fruechss <116127655+rhit-fruechss@users.noreply.github.com>
2023-04-27 10:36:05 -0400

    Created starter files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to the to-be Zenc app! :)"/>
+  <!-- <HelloWorld msg="Welcome to the to-be Zenc app! :)"/> -->
+  <BackgroundMenu/>
+  <ZenScreen/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import BackgroundMenu from './components/BackgroundMenu.vue';
+import ZenScreen from './components/ZenScreen.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    BackgroundMenu,
+    ZenScreen
+}
 }
 </script>
 
