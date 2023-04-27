e084c755cf9b4c1591e207f92d37bf2991954554
rhit-fruechss <116127655+rhit-fruechss@users.noreply.github.com>
2023-04-27 14:38:51 -0400

    Added serve.sh

---
+++ serve.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+tsc --project functions
+rm -rf public/*
+concurrently -k \
+            "firebase serve" \
+            "tsc --project functions --watch" \
+            "cd frontend && node_modules/.bin/vue-cli-service build --no-clean --dest ../public --watch" \
+            "cd frontend && node_modules/.bin/vue-cli-service serve" \
+            "sleep 10 && browser-sync start --proxy 'localhost:5000' --files 'public/*'"
